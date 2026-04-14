54bf36ea24211262867117063dcb633cdd9973db
Reina234 <zhengreina@gmail.com>
2026-04-14 10:04:36 +0100

    obabel refactor

--- src/polymer_md/conversion/file_formats.py
+++ src/polymer_md/conversion/file_formats.py
@@ -1,7 +1,19 @@
+from dataclasses import dataclass
 from enum import StrEnum
+
+from polymer_md.utils import FileHelper, PathType
 
 
 class FileFormats(StrEnum):
     PDB = "pdb"
     MOL2 = "mol2"
     GRO = "gro"
+
+
+@dataclass(frozen=True)
+class PathInput:
+    path: PathType
+
+    @property
+    def format(self) -> str:
+        return FileHelper.get_suffix_type(path=self.path)
--- src/polymer_md/conversion/obabel.py
+++ src/polymer_md/conversion/obabel.py
@@ -1,10 +1,11 @@
 import logging
 from pathlib import Path
-from typing import Literal, get_args
+from typing import Literal, Optional, Union, get_args
 
 from openbabel import pybel
 
 from polymer_md.conversion.file_formats import FileFormats
+from polymer_md.core.monomer import Monomer
 from polymer_md.utils.file import FileHelper, PathType
 
 logger = logging.getLogger(__name__)
@@ -15,51 +16,51 @@ OBABEL_OUTPUTS: set[str] = set(get_args(ObabelOutputs))
 
 
 class ObabelConverter:
-    @classmethod
-    def convert_smiles(
-        cls,
-        smiles: str,
-        output_dir: PathType,
-        output_name: str,
+    default_dir = Path("obabel_outputs/")
+
+    def __init__(self, item_to_convert: Union[PathType, Monomer]) -> None:
+        self._mol: pybel.Molecule = self._get_mol(item_to_convert=item_to_convert)
+        self.default_name: str = self._get_default_name(item_to_convert=item_to_convert)
+
+    def _get_mol(self, item_to_convert: Union[PathType, Monomer]) -> pybel.Molecule:
+        if isinstance(item_to_convert, Monomer):
+            return self._get_mol_object_from_smiles(smiles=item_to_convert.smiles)
+        if isinstance(item_to_convert, PathType):
+            return self._get_mol_object_from_path(input_path=item_to_convert)
+
+        raise ValueError(f"[UNSUPPORTED_INPUT_TYPE]:{type(item_to_convert)}")
+
+    def _get_default_name(self, item_to_convert: Union[PathType, Monomer]) -> str:
+        if isinstance(item_to_convert, Monomer):
+            return item_to_convert.label or item_to_convert.smiles
+        if isinstance(item_to_convert, PathType):
+            return Path(item_to_convert).stem
+
+        raise ValueError(f"[UNSUPPORTED_INPUT_TYPE]:{type(item_to_convert)}")
+
+    def convert(
+        self,
         output_format: ObabelOutputs,
+        output_dir: Optional[PathType] = None,
+        output_name: Optional[str] = None,
         overwrite: bool = False,
     ) -> Path:
         logger.info("Conversion started")
+        output_dir = output_dir or self.default_dir
+        output_name = output_name or self.default_name
         output_path = FileHelper.construct_path(
             path_dir=output_dir, stem=output_name, suffix=output_format
         )
-        cls._save_mol_object(
-            cls._get_mol_object_from_smiles(smiles=smiles),
+        self._save_mol_object(
+            self._mol,
             output_path=output_path,
             overwrite=overwrite,
         )
 
         return output_path
 
-    @classmethod
-    def convert_file(
-        cls,
-        input_path: PathType,
-        output_dir: PathType,
-        output_name: str,
-        output_format: ObabelOutputs,
-        overwrite: bool = False,
-    ) -> Path:
-        logger.info("Conversion started")
-        output_path = FileHelper.construct_path(
-            path_dir=output_dir, stem=output_name, suffix=output_format
-        )
-        cls._save_mol_object(
-            cls._get_mol_object_from_path(input_path=input_path),
-            output_path=output_path,
-            overwrite=overwrite,
-        )
-
-        return output_path
-
-    @classmethod
     def _save_mol_object(
-        cls,
+        self,
         mol_object: pybel.Molecule,
         output_path: PathType,
         overwrite: bool = False,
@@ -73,8 +74,7 @@ class ObabelConverter:
             output_path,
         )
 
-    @classmethod
-    def _get_mol_object_from_path(cls, input_path: PathType) -> pybel.Molecule:
+    def _get_mol_object_from_path(self, input_path: PathType) -> pybel.Molecule:
         input_format = FileHelper.safe_get_suffix_type(
             path=input_path, supported_suffixes=OBABEL_INPUTS
         )
@@ -85,8 +85,7 @@ class ObabelConverter:
         )
         return pybel_objects[0]
 
-    @classmethod
-    def _get_mol_object_from_smiles(cls, smiles: str) -> pybel.Molecule:
+    def _get_mol_object_from_smiles(self, smiles: str) -> pybel.Molecule:
         mol = pybel.readstring("smi", smiles)
         logger.info(
             "OBabel object read from %s",
